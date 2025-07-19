74c433323ea28a8afa9fe628ab813fe46b185af9
unknown <vansh.y.v.y@gmail.com>
2025-07-19 20:44:50 +0530

    Cropped image added successfully

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers.tracker import Tracker
+import cv2
 
 
 
@@ -13,6 +14,15 @@ def main():
     tracks=tracker.get_object_tracks(video_frames,
                                      read_from_snub=True,
                                      stub_path="C:/Users/Vansh/OneDrive/Desktop/football_analysis/stubs/tracks.pkl")
+    
+    for track_id, player in tracks['players'][0].items():
+         bbox = player
+         frame =video_frames[0]
+         cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+         cv2.imwrite(f"C:/Users/Vansh/OneDrive/Desktop/football_analysis/output_videos/cropped_image.jpg", cropped_image)
+        
+         break
+
     #draw object track
     output_video_frames= tracker.draw_annotations(video_frames,tracks)
 
